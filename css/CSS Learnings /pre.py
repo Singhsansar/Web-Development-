302009af4c4e079f9256f0c418dabc9fba1ba217
Singh <singhsansar433@gmail.com>
2022-12-16 19:51:11 +0530

    HTML and CSS

---
+++ css/CSS Learnings /pre.py
@@ -0,0 +1,22 @@
+def preorder(root): 
+    if root is not None: 
+        print(root.val) 
+        preorder(root.left) 
+        preorder(root.right)
+
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p is None and q is None:
+            return True 
+        if p is None or q is None: 
+            return False
+        return (p.val == q.val) and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+
+
